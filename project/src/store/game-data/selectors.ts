htmlacademy/guess-melody-demo

8.8. Объединит слайсы в один редьюсер. WIP
Объединим слайсы в общий редьюсер. Для этого создадим файл
`root-reducer.ts`. Затем подключим его при инициализации хранилища.

Сделаем ещё один небольшой рефакторинг. Вместо прямого обращения к
полям хранилища воспользуемся селекторами.

Селектор — обычная функция, которая возвращает значение нужного
поля хранилища.

WIP.  Проект по-прежнему не работает. Мы подготовили всё необходимое,
но не обновили компоненты.

project/src/store/game-data/selectors.ts
@@ -1,0 +1,6 @@
+import {NameSpace} from '../../const';
+import {State} from '../../types/state';
+import {Questions} from '../../types/question';
+
+export const getQuestions = (state: State): Questions => state[NameSpace.data].questions;
+export const getLoadedDataStatus = (state: State): boolean => state[NameSpace.data].isDataLoaded;

konovalov-as/guess-melody-10

project/src/store/game-data/selectors.ts
@@ -1,0 +1,6 @@
+import {NameSpace} from '../../const';
+import {State} from '../../types/state';
+import {Questions} from '../../types/question';
+
+export const getQuestions = (state: State): Questions => state[NameSpace.Data].questions;
+export const getLoadedDataStatus = (state: State): boolean => state[NameSpace.Data].isDataLoaded;